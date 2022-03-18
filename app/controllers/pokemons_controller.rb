5eebf1d00aad963abde7bde92d8c42c078f2b8d7
Olivier Fortin <olivier@zap.qc.ca>
2022-03-18 19:45:45 -0400

    Added new page to show the paginated table

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,5 +1,6 @@
 class PokemonsController < ApplicationController
-  
+  require 'csv'
+
   def index
     @pokemons = Pokemon.all
     render json: @pokemons
@@ -11,7 +12,7 @@ class PokemonsController < ApplicationController
       if @pokemon
         render json: @pokemon, status: :ok
       else
-        render json: { error: 'Unable to get pokemon'}, status: 400
+        render json: { error: 'Unable to get pokemon'}, status: :bad_request
       end
     rescue ActiveRecord::RecordNotFound
       head :not_found
@@ -34,7 +35,7 @@ class PokemonsController < ApplicationController
         @pokemon.update(pokemon_params)
         render json: @pokemon
       else
-        render json: { error: 'Unable to update pokemon'}, status: 400
+        render json: { error: 'Unable to update pokemon'}, status: :bad_request
       end
     rescue ActiveRecord::RecordNotFound
       head :not_found
@@ -48,11 +49,42 @@ class PokemonsController < ApplicationController
         @pokemon.destroy()
         head :no_content
       else
-        render json: { error: 'Unable to  destroy pokemon'}, status: 400
+        render json: { error: 'Unable to  destroy pokemon'}, status: :bad_request
       end
     rescue ActiveRecord::RecordNotFound
       head :not_found
     end
+  end
+
+  def load_csv
+    # Original csv was found there: https://gist.github.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6
+    # Method preserved for history purposes but should not because it isn't restful
+      # pokemons = CSV.foreach('resources/pokemon.csv', headers: true).map do |row|                                                                 
+      # { 
+      #   number: row['Number'].to_i,
+      #   name: row['Name'],
+      #   type_1: row['Type_1'],
+      #   type_2: row['Type_2'],
+      #   total: row['Total'].to_i,
+      #   hp: row['HP'].to_i,
+      #   attack: row['Attack'].to_i,
+      #   defense: row['Defense'].to_i,
+      #   sp_atk: row['Sp_Atk'].to_i,
+      #   sp_def: row['Sp_Def'].to_i,
+      #   speed: row['Speed'].to_i,
+      #   generation: row['Generation'].to_i,
+      #   legendary: (row['Legendary'].eql? 'True')
+      # }
+      # end
+      # Pokemon.insert_all(pokemons)
+      # render json: pokemons
+      render json: { error: 'This method should not be called anymore' }, status: :bad_request
+  end
+
+  def table
+    # content_tag(:h1, 'Pokemon table on Rails')
+    # content_tag(:p, 'Page number '+:page)
+    # render
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # Uncomment if the database has been dropped and needs data again
+  # post 'pokemons/action/load', to: 'pokemons#load_csv'
   resources :pokemons
+  get 'pokemons/visual/table/:page', to: 'pokemons#table'
 end
